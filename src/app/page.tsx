10de058a8cb2e7ab1181053c6e3610006b499d51
nuruddin <sg7720@gmail.com>
2024-12-07 18:12:58 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React, { useRef } from 'react';
+import styles from "./basic.module.scss"
+import Image from "next/image";
+import myPic from "@/app/me2.jpeg";
+import Link from "next/link";
+
+
+const CubePage: React.FC = () => {
+    return (
+        <div>
+           HELLO
+        </div>
+    );
+};
+
+export default CubePage;
